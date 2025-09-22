d3974ec006ccfc0678a9696c66fd5b9bf2b04690
Rakshit Tyagi <rakshit.t@supplyvalid.com>
2025-09-22 15:19:22 +0530

    updated

---
+++ metro.config.js
@@ -0,0 +1,14 @@
+// // Learn more https://docs.expo.io/guides/customizing-metro
+// const { getDefaultConfig } = require('expo/metro-config');
+
+// /** @type {import('expo/metro-config').MetroConfig} */
+// const config = getDefaultConfig(__dirname);
+
+// module.exports = config;
+
+
+
+// metro.config.js
+const { getDefaultConfig } = require('@react-native/metro-config');
+
+module.exports = getDefaultConfig(__dirname);
